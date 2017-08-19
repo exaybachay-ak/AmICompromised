93e66922b5a1a17e42e041c11d4037e55ef44e93
Jacob Kelley <exaybachay.ak@gmail.com>
2017-08-18 22:20:32 -0800

    Delete hnids.py

--- hnids.py
+++
@@ -1,8 +0,0 @@
-#make sure you have scapy installed!
-
-from scapy.all import *
-
-def packet_callback(packet):
-    print packet.show()
-
-p = sniff(iface='eth0', timeout=1000, prn=packet_callback)
